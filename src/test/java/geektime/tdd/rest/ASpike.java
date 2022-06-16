da0fa8a6b880c3ddd03ebe62a8b14033141a83cf
James Zhang <liulfbaby@gmail.com>
2022-06-16 17:02:28 +0800

    init with simple test

---
+++ src/test/java/geektime/tdd/rest/ASpike.java
@@ -0,0 +1,12 @@
+package geektime.tdd.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ASpike {
+    @Test
+    public void should() {
+        assertTrue(true);
+    }
+}
